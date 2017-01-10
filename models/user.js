c59e14437feafe88a7c7a6b597d90a0eb84bce14
RvDeveloper <avadhesh_bhatt@rvtechnologies.c.in>
2017-01-10 10:55:29 +0530

    model changes

--- models/user.js
+++ models/user.js
@@ -58,19 +58,19 @@ var userSchema = mongoose.Schema({
         token        : String,
         email        : String,
         name         : String
-    },
-    twitter          : {
-        id           : String,
-        token        : String,
-        displayName  : String,
-        username     : String
-    },
-    google           : {
-        id           : String,
-        token        : String,
-        email        : String,
-        name         : String
     }
+//    twitter          : {
+//        id           : String,
+//        token        : String,
+//        displayName  : String,
+//        username     : String
+//    },
+//    google           : {
+//        id           : String,
+//        token        : String,
+//        email        : String,
+//        name         : String
+//    }
 
 });
 
