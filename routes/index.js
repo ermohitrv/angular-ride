b0f6bbf46556fd90865df44717778e35310cc14d
preeti <preeti_dev@rvtechnologies.co.in>
2017-03-02 18:34:20 +0530

    search form

--- routes/index.js
+++ routes/index.js
@@ -932,7 +932,6 @@ router.get('/searchProducts', function(req, res){
         res.header('Cache-Control', 'no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0');
         res.render('index', { user : req.user,title:'Homepage' });
         
-        
     }
   
     
