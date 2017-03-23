5acf5355be2d13c8b4f084c7b3af79fdbba94bc7
preeti <preeti_dev@rvtechnologies.co.in>
2017-03-23 17:30:33 +0530

    nearby riders

--- routes/api.js
+++ routes/api.js
@@ -2266,7 +2266,7 @@ router.post('/nearby-riders', function (req, res) {
     //var email               = 'preeti_dev@rvtechnologies.co.in';
     //var currentlocationLat  = '1.2393';
     //var currentlocationLng  = '1.8184';
-    var jsonarray = "";
+    var jsonarray = [];
     
     if(email != "" && email != undefined && currentlocationLat != "" && currentlocationLat != undefined && currentlocationLng != "" && currentlocationLng != undefined){
     RpRoutes.find({}, function(err, getrproutes){
@@ -2363,9 +2363,8 @@ router.post('/nearby-riders', function (req, res) {
                                     });
                             }
                             else{
-                                jsonarray = user;
-                                //jsonarray.user
-                                //jsonarray.push(user);
+                                //jsonarray = user;
+                                jsonarray.push(user);
 //                                res.json({ 
 //                                    success: true,
 //                                    data:{user:user},
